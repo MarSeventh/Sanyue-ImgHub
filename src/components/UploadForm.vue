8e169c552829621a191a340de5a9fe858d8e5e07
MarSeventh <1193267292@qq.com>
2024-07-16 10:49:44 +0800

    完善前端

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -2,12 +2,14 @@
     <div class="upload-form">
         <el-upload
             class="upload-card"
+            :class="{'is-uploading': uploading}"
             drag
             :http-request="uploadFile"
             multiple
             :onSuccess="handleSuccess"
             :on-exceed="handleExceed"
             :on-error="handleError"
+            :before-upload="beforeUpload"
             :file-list="fileList"
             :show-file-list="false"
             :limit="10"
@@ -23,27 +25,29 @@
         <el-card class="upload-list-card" v-if="fileList.length">
             <div class="upload-list-container">
                 <el-scrollbar>
-                    <el-row class="upload-list-item" v-for="file in fileList" :key="file.name" :span="8">
-                        <el-image
-                            style="width: 20%; height: 32%;border-radius: 12px;"
+                    <div class="upload-list-item" v-for="file in fileList" :key="file.name" :span="8">
+                        <img
+                            style="width: 10vw; border-radius: 12px;"
                             :src="file.url"
-                            fit="cover">
-                        </el-image>
+                            >
+                        </img>
                         <div class="upload-list-item-content">
                             <div class="upload-list-item-name">{{ file.name }}</div>
                             <div class="upload-list-item-url">
-                                <el-link :underline="false" :href="file.url" target="_blank">{{ file.url }}</el-link>
-                            </div>
-                            <div class="upload-list-item-action">
-                                <el-button type="text" @click="handleCopy(file)">
-                                    <el-icon><Link /></el-icon>复制链接
-                                </el-button>
-                                <el-button type="text" @click="handleRemove(file)">
-                                    <el-icon><Delete /></el-icon>删除
-                                </el-button>
+                                <el-link :underline="false" :href="file.url" target="_blank">
+                                    <el-text class="upload-list-item-url-text" truncated>{{ file.url }}</el-text>
+                                </el-link>
                             </div>
                         </div>
-                    </el-row>
+                        <div class="upload-list-item-action">
+                                <el-button type="primary" round size="small" class="upload-list-item-action-button" @click="handleCopy(file)">
+                                    <el-icon><Link /></el-icon>
+                                </el-button>
+                                <el-button type="danger" round size="small" class="upload-list-item-action-button" @click="handleRemove(file)">
+                                    <el-icon><Delete /></el-icon>
+                                </el-button>
+                        </div>
+                    </div>
                 </el-scrollbar>
             </div>
         </el-card>
@@ -56,15 +60,27 @@ export default {
 name: 'UploadForm',
 data() {
     return {
-        fileList: []
+        fileList: [
+            {
+                uid: 1,
+                name: 'image.png',
+                url: 'https://imgbed.sanyue.site/file/0dbd5add3605a0b2e8994.jpg'
+            },
+            {
+                uid: 2,
+                name: 'video.mp4',
+                url: 'https://imgbed.sanyue.site/file/0dbd5add3605a0b2e8994.jpg'
+            }
+        ],
+        uploading: false,
+        file: null
     }
 },
 methods: {
     uploadFile(file) {
-        console.log(file)
         const formData = new FormData()
         formData.append('file', file.file)
-        return axios.post('https://imgbed.sanyue.site/upload', formData)
+        return axios.post('/upload', formData)
     },
     handlePreview(file) {
         window.open(file.url, '_blank')
@@ -73,7 +89,7 @@ methods: {
         this.fileList = this.fileList.filter(item => item.uid !== file.uid)
         this.$message({
             type: 'info',
-            message: '已删除'
+            message: file.name + '已删除'
         })
     },
     handleSuccess(response, file) {
@@ -85,19 +101,21 @@ methods: {
             })
             this.$message({
                 type: 'success',
-                message: '上传成功'
+                message: file.name + '上传成功'
             })
-            console.log(file)
         } catch (error) {
-            this.$message.error('上传失败')
+            this.$message.error(file.name + '上传失败')
             this.fileList = this.fileList.filter(item => item.uid !== file.uid)
+        } finally {
+            this.uploading = false
         }
     },
     handleExceed(files) {
         this.$message.warning(`当前限制选择 10 个文件，本次选择了 ${files.length} 个文件`)
     },
     handleError(err) {
-        this.$message.error('上传失败')
+        this.$message.error(this.file.name + '上传失败')
+        this.uploading = false
     },
     handleCopy(file) {
         navigator.clipboard.writeText(file.url)
@@ -105,12 +123,30 @@ methods: {
             type: 'success',
             message: '复制成功'
         })
+    },
+    beforeUpload(file) {
+        const isLt5M = file.size / 1024 / 1024 < 5
+        if (!isLt5M) {
+            this.$message.error('上传文件大小不能超过 5MB!')
+        } else {
+            this.uploading = true
+            this.file = file
+        }
+        return isLt5M
     }
 }
 }
 </script>
 
 <style scoped> 
+@keyframes breathe {
+    0%, 100% {
+    }
+    50% {
+        box-shadow: 0 0 10px 5px #409EFF;
+        opacity: 0.8;
+    }
+}
 .upload-form {
     display: flex;
     flex-direction: column;
@@ -118,8 +154,8 @@ methods: {
     align-items: center;
 }
 .upload-list-card {
-    width: 35rem;
-    height: 12rem;
+    width: 55vw;
+    height: 30vh;
     margin-top: 10px;
     display: flex;
     flex-direction: column;
@@ -127,15 +163,17 @@ methods: {
     justify-content: center;
     border-radius: 15px;
     opacity: 0.8;
+    background-color: rgba(255, 255, 255, 0.7);
     backdrop-filter: blur(10px);
 }
 .upload-list-container {
-    width: 35rem;
-    height: 12rem;
+    width: 55vw;
+    height: 30vh;
 }
 .upload-list-item {
     display: flex;
     align-items: center;
+    justify-content: space-between;
     margin: 5px;
     border: 1px solid #a5bef7;
     padding: 5px;
@@ -150,11 +188,19 @@ methods: {
     flex-direction: column;
     margin-left: 10px;
 }
-.upload-list-item-url {
-    margin-top: 3px;
+.upload-list-item-url-text {
+    width: 28vw;
+}
+.upload-list-item-action {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.upload-list-item-action-button {
+    margin: 2px;
 }
 .upload-card {
-    width: 35rem;
+    width: 55vw;
     padding: 20px;
     background: none;
 }
@@ -163,10 +209,11 @@ methods: {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 18rem;
+    height: 30vh;
     border-radius: 15px;
     border: 3px dashed #409EFF;
-    opacity: 0.6;
+    opacity: 0.7;
+    background-color: rgba(255, 255, 255, 0.6);
     backdrop-filter: blur(10px);
     transition: all 0.3s ease;
 }
@@ -177,6 +224,9 @@ methods: {
 /deep/ .el-upload-dragger.is-dragover {
     opacity: 0.8;
     box-shadow: 0 0 10px 5px #409EFF;
+}
+.is-uploading /deep/ .el-upload-dragger{
+    animation: breathe 3s infinite;
 }
 .el-upload__text {
     font-weight: bold;
